376453ef74c1ecc4a6a55c69af625eb13a09596f
wojwoj8 <wojwoj8@gmail.com>
2023-04-04 14:55:55 +0200

    attack and place ship works

--- src/logic.js
+++ src/logic.js
@@ -1,7 +1,7 @@
 const Ship = (length) => {
   const sLength = () => length;
   let sunk = false;
-
+  const getLength = () => length;
   const hitTable = [];
   const isSunk = () => sunk;
   const showHitTable = () => hitTable;
@@ -19,33 +19,66 @@ const Ship = (length) => {
   hitFields();
 
   return {
-    sLength, gotHit, isSunk, showHitTable,
+    sLength, gotHit, isSunk, showHitTable, getLength,
   };
 };
 
 const Gameboard = () => {
   const coordinatesTab = Array(10).fill().map(() => Array(10).fill(0));
-  const status = () => coordinatesTab;
+  const table = () => coordinatesTab;
+  const shipsData = [];
+  const getShipsData = () => shipsData;
 
-  const placeShip = (length, x, y) => {
-    const ship = Ship(length, 0, false);
+  const placeShip = (name, length, x, y) => {
+    const ship = Ship(length);
 
-    // place on gameboard coordinates and where ship coordinates = 1
-    for (let i = length; i > 0; i--) {
-      coordinatesTab[x][y] = 1;
+    // place on gameboard coordinates ship name and ship field property (hit:true/false)
+    for (let i = length - 1; i >= 0; i--) {
+      // console.log(ship.showHitTable()[i].hit);
+      coordinatesTab[x][y] = {
+        name,
+        hit: ship.showHitTable()[i].hit,
+        index: i,
+      };
       x++;
     }
+    shipsData.push({
+      name,
+      data: ship,
+
+    });
   };
   const receiveAttack = (x, y) => {
-    let tab = coordinatesTab[x][y];
+    const tab = table()[x][y];
+    // if missed
     if (tab === 0) {
-      tab = 2;
-    } else if (tab === 1) {
-      // s
+      // console.log('miss');
+      // console.log(tab);
+      table()[x][y] = 2;
+      // console.log(tab);
+      // console.log(table()[x][y]);
+      // console.log(tab);
+    } else if (typeof tab === 'object') {
+      const shipName = tab.name;
+      // if there is more than 1 ships it finds under which index hides our attacked ship
+      const index = shipsData.findIndex((element) => element.name === shipName);
+      const shipAttackedFieldIndex = tab.index;
+      // console.log(getShipsData()[index]);
+      // console.log(getShipsData()[index].data.showHitTable());
+
+      // change hit to true in shipsData
+      getShipsData()[index].data.gotHit(shipAttackedFieldIndex);
+      // console.log(getShipsData()[index].data.isSunk());
+      // change hit to true in coordinates
+      tab.hit = getShipsData()[index].data.showHitTable()[shipAttackedFieldIndex].hit;
+      // console.log(getShipsData()[index].data.isSunk());
+      // console.log(tab);
     }
   };
 
-  return { status, placeShip };
+  return {
+    table, placeShip, getShipsData, receiveAttack,
+  };
 };
 
 export { Ship, Gameboard };
--- src/logic.test.js
+++ src/logic.test.js
@@ -1,7 +1,5 @@
 const logic = require('./logic');
 
-const board = logic.Gameboard();
-
 const ship = logic.Ship(3);
 
 describe('ship', () => {
@@ -11,6 +9,7 @@ describe('ship', () => {
 
   test('ship hit table', () => {
     const tab = ship.showHitTable();
+    // console.log(tab[1].hit);
     expect(tab).toEqual([{ hit: false }, { hit: false }, { hit: false }]);
   });
 
@@ -29,6 +28,58 @@ describe('ship', () => {
 
 describe('gameboard', () => {
   test('gameboard size', () => {
-    expect(board.status().length && board.status()[0].length).toBe(10);
+    const board = logic.Gameboard();
+    expect(board.table().length && board.table()[0].length).toBe(10);
+  });
+
+  test('place ship', () => {
+    const board = logic.Gameboard();
+    const statek1 = board.placeShip('statek1', 3, 1, 2);
+    // console.log(board.getShipsData()[0].data.getLength());
+    expect(board.getShipsData()[0].name).toContain('statek1');
+    expect(board.getShipsData()[0].data.getLength()).toBe(3);
+  });
+
+  test('placed ship coordinates', () => {
+    const board = logic.Gameboard();
+    const statek = board.placeShip('statek1', 5, 2, 4);
+    // console.log(board.table());
+    expect(board.table()[2][4]).toEqual({ hit: false, name: 'statek1', index: 4 });
+    expect(board.table()[3][4]).toEqual({ hit: false, name: 'statek1', index: 3 });
+    expect(board.table()[4][4]).toEqual({ hit: false, name: 'statek1', index: 2 });
+    expect(board.table()[5][4]).toEqual({ hit: false, name: 'statek1', index: 1 });
+    expect(board.table()[6][4]).toEqual({ hit: false, name: 'statek1', index: 0 });
+    expect(board.table()[7][4]).toEqual(0);
+  });
+
+  test('placed ship attacked', () => {
+    const board = logic.Gameboard();
+    const statek = board.placeShip('statek1', 5, 2, 4);
+    // console.log(board.table());
+    expect(board.table()[2][4]).toEqual({ hit: false, name: 'statek1', index: 4 });
+    board.receiveAttack(2, 4);
+    // console.log(board.table()[2][4]);
+    expect(board.table()[2][4]).toEqual({ hit: true, name: 'statek1', index: 4 });
+    expect(board.table()[4][4]).toEqual({ hit: false, name: 'statek1', index: 2 });
+  });
+
+  test('placed ship sunk', () => {
+    const board = logic.Gameboard();
+    const statek = board.placeShip('statek1', 2, 1, 5);
+    board.receiveAttack(1, 5);
+    expect(board.getShipsData()[0].data.isSunk()).toEqual(false);
+    board.receiveAttack(2, 5);
+    expect(board.table()[1][5]).toEqual({ hit: true, name: 'statek1', index: 1 });
+    expect(board.table()[2][5]).toEqual({ hit: true, name: 'statek1', index: 0 });
+    // console.log(board.getShipsData()[0].data.isSunk());
+    // expect(board.getShipsData()[0].data.isSunk()).toEqual(true);
+  });
+  test('missed attack', () => {
+    const board = logic.Gameboard();
+    const statek = board.placeShip('statek1', 2, 1, 5);
+    board.receiveAttack(1, 6);
+    // console.log(board.table()[1][6]);
+    expect(board.table()[1][6]).toEqual(2);
+    expect(board.table()[1][7]).toEqual(0);
   });
 });
